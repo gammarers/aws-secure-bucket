4d4db37889053292bca2cc5cf60cd8f521ac3deb
yicr <yicr@users.noreply.github.com>
2023-01-30 11:03:48 +0900

    feat!: first development.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,45 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export interface SecureBucketProps {
+  readonly bucketName?: string;
+  readonly encryption?: SecureBucketEncryption;
+  readonly versioned?: boolean;
+}
+
+export enum SecureBucketEncryption {
+  /**
+   * Server-side KMS encryption with a master key managed by KMS.
+   */
+  KMS_MANAGED = 'KMS_MANAGED',
+  /**
+   * Server-side encryption with a master key managed by S3.
+   */
+  S3_MANAGED = 'S3_MANAGED',
+}
+
+export class SecureBucket extends s3.Bucket {
+  constructor(scope: Construct, id: string, props?: SecureBucketProps) {
+    super(scope, id, {
+      bucketName: props?.bucketName,
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      encryption: (() => {
+        if (props?.encryption) {
+          switch (props.encryption) {
+            case SecureBucketEncryption.KMS_MANAGED:
+              return s3.BucketEncryption.KMS_MANAGED;
+            case SecureBucketEncryption.S3_MANAGED:
+              return s3.BucketEncryption.S3_MANAGED;
+          }
+        }
+        return s3.BucketEncryption.KMS_MANAGED;
+      })(),
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
+      versioned: props?.versioned ? props.versioned : true,
+    });
   }
 }
---
+++ test/bucket.test.ts
@@ -0,0 +1,170 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { SecureBucket, SecureBucketEncryption } from '../src';
+
+describe('SecureBucket Testing', () => {
+
+  describe('SecureBucket Default Parameters Testing', () => {
+    const app = new App();
+    const stack = new Stack(app, 'TestingStack', {
+      env: {
+        account: '123456789012',
+        region: 'us-east-1',
+      },
+    });
+
+    const bucket = new SecureBucket(stack, 'SecureBucket', {
+      bucketName: 'example-secure-bucket',
+    });
+
+    it('Is Bucket', () => {
+      expect(bucket).toBeInstanceOf(s3.Bucket);
+    });
+
+    const template = Template.fromStack(stack);
+
+    it('Should have encryption', () => {
+      template.hasResourceProperties('AWS::S3::Bucket', {
+        BucketEncryption: Match.objectEquals({
+          ServerSideEncryptionConfiguration: [
+            {
+              ServerSideEncryptionByDefault: {
+                SSEAlgorithm: 'aws:kms',
+              },
+            },
+          ],
+        }),
+      });
+    });
+
+    it('Should block public access', () => {
+      template.hasResourceProperties('AWS::S3::Bucket', {
+        PublicAccessBlockConfiguration: Match.objectEquals({
+          BlockPublicAcls: true,
+          BlockPublicPolicy: true,
+          IgnorePublicAcls: true,
+          RestrictPublicBuckets: true,
+        }),
+      });
+    });
+
+    it('Should versioning enabled', () => {
+      template.hasResourceProperties('AWS::S3::Bucket', {
+        VersioningConfiguration: Match.objectEquals({
+          Status: 'Enabled',
+        }),
+      });
+    });
+
+    it('Should enforce SSL', () => {
+      template.hasResourceProperties('AWS::S3::BucketPolicy', {
+        PolicyDocument: {
+          Version: '2012-10-17',
+          Statement: Match.arrayWith([
+            Match.objectLike({
+              Action: 's3:*',
+              Condition: {
+                Bool: {
+                  'aws:SecureTransport': 'false',
+                },
+              },
+              Effect: 'Deny',
+              Principal: {
+                AWS: '*',
+              },
+            }),
+          ]),
+        },
+      });
+    });
+    it('Should match snapshot', () => {
+      expect(template.toJSON()).toMatchSnapshot('default-secure-bucket');
+    });
+  });
+  describe('SecureBucket Encryption S3 Managed Testing', () => {
+    const app = new App();
+    const stack = new Stack(app, 'TestingStack');
+    const bucket = new SecureBucket(stack, 'SecureBucket', {
+      bucketName: 'example-secure-bucket',
+      encryption: SecureBucketEncryption.S3_MANAGED,
+    });
+
+    it('Is Bucket', () => {
+      expect(bucket).toBeInstanceOf(s3.Bucket);
+    });
+
+    const template = Template.fromStack(stack);
+
+    it('Should have encryption', () => {
+      template.hasResourceProperties('AWS::S3::Bucket', {
+        BucketEncryption: Match.objectEquals({
+          ServerSideEncryptionConfiguration: [
+            {
+              ServerSideEncryptionByDefault: {
+                SSEAlgorithm: 'AES256',
+              },
+            },
+          ],
+        }),
+      });
+    });
+
+    it('Should match snapshot', () => {
+      expect(template.toJSON()).toMatchSnapshot('encryption-s3-managed-bucket');
+    });
+  });
+
+  describe('SecureBucket Encryption KMS Managed Testing', () => {
+    const app = new App();
+    const stack = new Stack(app, 'TestingStack');
+    const bucket = new SecureBucket(stack, 'SecureBucket', {
+      bucketName: 'example-secure-bucket',
+      encryption: SecureBucketEncryption.KMS_MANAGED,
+    });
+
+    it('Is Bucket', () => {
+      expect(bucket).toBeInstanceOf(s3.Bucket);
+    });
+
+    const template = Template.fromStack(stack);
+
+    it('Should have encryption', () => {
+      template.hasResourceProperties('AWS::S3::Bucket', {
+        BucketEncryption: Match.objectEquals({
+          ServerSideEncryptionConfiguration: [
+            {
+              ServerSideEncryptionByDefault: {
+                SSEAlgorithm: 'aws:kms',
+              },
+            },
+          ],
+        }),
+      });
+    });
+
+    it('Should match snapshot', () => {
+      expect(template.toJSON()).toMatchSnapshot('encryption-kms-managed-bucket');
+    });
+  });
+
+  describe('SecureBucket No Versioning Testing', () => {
+    const app = new App();
+    const stack = new Stack(app, 'TestingStack');
+    const bucket = new SecureBucket(stack, 'SecureBucket', {
+      bucketName: 'example-secure-bucket',
+      encryption: SecureBucketEncryption.KMS_MANAGED,
+      versioned: false,
+    });
+
+    it('Is Bucket', () => {
+      expect(bucket).toBeInstanceOf(s3.Bucket);
+    });
+
+    const template = Template.fromStack(stack);
+
+    it('Should match snapshot', () => {
+      expect(template.toJSON()).toMatchSnapshot('no-versioning-bucket');
+    });
+  });
+});
