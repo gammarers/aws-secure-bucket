31047e61eb06dfec3105d7315fe014b8016c710e
yicr <yicr@users.noreply.github.com>
2023-03-22 15:02:01 +0900

    feat!: node version changed(16)

--- .projenrc.ts
+++ .projenrc.ts
@@ -10,8 +10,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/yicr/secure-bucket.git',
   keywords: ['aws', 'cdk', 'aws-cdk', 's3', 'bucket'],
   npmAccess: javascript.NpmAccess.PUBLIC,
-  minNodeVersion: '18.0.0',
-  workflowNodeVersion: '18.15.0',
+  minNodeVersion: '16.0.0',
+  workflowNodeVersion: '16.19.1',
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
