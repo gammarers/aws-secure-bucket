9bdffd2e98de1b3f536aa73c5ca1b17e56e4a8c2
yicr <yicr@users.noreply.github.com>
2023-08-23 15:34:38 +0900

    feat!: cdk version to 2.80.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
-  cdkVersion: '2.61.0',
+  cdkVersion: '2.80.0',
   typescriptVersion: '5.0.x',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-secure-bucket',
