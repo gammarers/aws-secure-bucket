0e6ddd33257f8f0b45e671817a5273b38d90fe50
yicr <yicr@users.noreply.github.com>
2023-09-15 15:03:36 +0900

    chore: node mine version set to 16.0.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -13,7 +13,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   keywords: ['aws', 'cdk', 'aws-cdk', 's3', 'bucket', 'secure', 'kms'],
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
-  minNodeVersion: '18.0.0',
+  minNodeVersion: '16.0.0',
   workflowNodeVersion: '18.17.1',
   depsUpgradeOptions: {
     workflowOptions: {
